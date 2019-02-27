3d370887c5d66bbe49f1c5975632dd5a8f4b51f4
Warren Matthews <warren@ourzoo.us>
2019-02-27 13:54:24 -0500

    Simple GET client. This works.

---
+++ get_client.py
@@ -0,0 +1,10 @@
+# Several package for a REST client. Pick one.
+# https://stackoverflow.com/questions/17301938/making-a-request-to-a-restful-api-using-python
+
+import requests
+import json
+
+url = 'http://192.168.1.36:5000/user/Nicholas'
+
+response = requests.get(url)
+print response.text
